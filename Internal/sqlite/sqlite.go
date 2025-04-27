285d0b9079cac0e07d1b215cbee6b2d1838a953e
anshul <anshulbhargav88@gmail.com>
2025-04-27 15:42:25 +0530

    add students list

--- Internal/sqlite/sqlite.go
+++ Internal/sqlite/sqlite.go
@@ -13,9 +13,9 @@ type Sqlite struct {
 	Db *sql.DB
 }
 
-func New(cfg *config.Config)(*Sqlite, error){
+func New(cfg *config.Config) (*Sqlite, error) {
 	db, err := sql.Open("sqlite3", cfg.StoragePath)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
@@ -26,7 +26,7 @@ func New(cfg *config.Config)(*Sqlite, error){
 	age INTEGER
 	)`)
 
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 
@@ -36,31 +36,30 @@ func New(cfg *config.Config)(*Sqlite, error){
 
 }
 
-func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error){
-    
+func (s *Sqlite) CreateStudent(name string, email string, age int) (int64, error) {
+
 	stmt, err := s.Db.Prepare("INSERT INTO students (name, email, age) VALUES (?, ?, ?)")
-	if err != nil{
+	if err != nil {
 		return 0, nil
 	}
 	defer stmt.Close()
 
 	result, err := stmt.Exec(name, email, age)
-	if err != nil{
+	if err != nil {
 		return 0, nil
 	}
 
 	lastId, err := result.LastInsertId()
-	if err != nil{
+	if err != nil {
 		return 0, nil
 	}
-
 
 	return lastId, nil
 }
 
-func (s *Sqlite) GetStudentById(id int64) (types.Student, error){
+func (s *Sqlite) GetStudentById(id int64) (types.Student, error) {
 	stmt, err := s.Db.Prepare("SELECT id, name, email, age FROM students WHERE id = ? LIMIT 1")
-	if err != nil{
+	if err != nil {
 		return types.Student{}, err
 	}
 
@@ -70,9 +69,8 @@ func (s *Sqlite) GetStudentById(id int64) (types.Student, error){
 
 	err = stmt.QueryRow(id).Scan(&student.Id, &student.Name, &student.Email, &student.Age)
 
-
-	if err != nil{
-		if err == sql.ErrNoRows{
+	if err != nil {
+		if err == sql.ErrNoRows {
 			return types.Student{}, fmt.Errorf("no student found with id %s", fmt.Sprint(id))
 		}
 		return types.Student{}, fmt.Errorf("queary error: %w", err)
@@ -80,5 +78,36 @@ func (s *Sqlite) GetStudentById(id int64) (types.Student, error){
 
 	return student, nil
 
+}
+
+func (s *Sqlite) GetStudents() ([]types.Student, error) {
+	stmt, err := s.Db.Prepare("SELECT id, name, email, age FROM students")
+	if err != nil {
+		return nil, err
+
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next() {
+		var student types.Student
+
+		err := rows.Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+		if err != nil {
+			return nil, err
+		}
+
+		students = append(students, student)
+
+	}
+	return students, nil
 
 }
